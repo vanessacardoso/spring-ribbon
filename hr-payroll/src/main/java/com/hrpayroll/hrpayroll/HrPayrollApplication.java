99d8ab4248b1da24850adfa1f49fa6e4649c1726
vanessa.cardoso <vanessacardoso.santos@gmail.com>
2021-05-09 17:01:21 -0300

    Balanceamento de carga com ribbon

---
+++ hr-payroll/src/main/java/com/hrpayroll/hrpayroll/HrPayrollApplication.java
@@ -0,0 +1,17 @@
+package com.hrpayroll.hrpayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@RibbonClient(name = "hr-worker")
+@EnableFeignClients
+@SpringBootApplication
+public class HrPayrollApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrPayrollApplication.class, args);
+    }
+
+}
